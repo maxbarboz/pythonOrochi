91568a993aa97384de63ae0183fe68025a7c6721
Maxsuel Storch <maxsuelstorch@gmail.com>
2020-02-05 17:11:18 -0300

    commit inicial

---
+++ comeco.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Esse é o comentário
+print ("Hello world!")
+
+print("Teste aqui ou não")
+
+number = 2 #Variável
+
+print(2+number)
+print(2-2)
+print(number/2)
+print(2*2)
+print(number**3)
+print(10/3)
+
+#Variáveis
+
+#Numérico inteiro
+numberInteiro = 47
+
+#Numérico flutuante
+numberFlutuante = 12.89
+
+#Textual
+varString = "Olá mundo!"
+
+#Boolean
+varTrue = True
+varFalse = False
+
+print(numberInteiro)
+print(numberFlutuante)
+print(varString)
+print(varTrue)
+print(varFalse)
